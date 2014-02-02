6499cce846c01f53b61c5b7a78512b44d99a891b
ElectricMaxxx <maximilian.berghoff@gmx.de>
2014-02-02 22:07:25 +0100

    let the content admin class inherit from the static content one

--- Admin/SeoAwareContentAdminController.php
+++ Admin/SeoAwareContentAdminController.php
@@ -2,45 +2,17 @@
 
 namespace Cmf\SeoBundle\Admin;
 
-use Sonata\AdminBundle\Datagrid\DatagridMapper;
-use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
-use Sonata\DoctrinePHPCRAdminBundle\Admin\Admin;
-use Symfony\Cmf\Bundle\RoutingBundle\Doctrine\Phpcr\Route;
+use Symfony\Cmf\Bundle\ContentBundle\Admin\StaticContentAdmin;
 
-class SeoAwareContentAdminController extends Admin{
+class SeoAwareContentAdminController extends StaticContentAdmin
+{
 
     protected $translationDomain = 'CmfSeoBundle';
 
     protected $baseRouteName = 'cmf_seo';
 
     protected $baseRoutePattern = 'seo_content';
-
-    public function getNewInstance()
-    {
-        $new = parent::getNewInstance();
-        if ($this->hasRequest()) {
-            $parentId = $this->getRequest()->query->get('parent');
-            if (null !== $parentId) {
-                $new->setParent($this->getModelManager()->find(null, $parentId));
-            }
-        }
-
-        return $new;
-    }
-
-    public function getExportFormats()
-    {
-        return array();
-    }
-
-    protected function configureListFields(ListMapper $listMapper)
-    {
-        $listMapper
-            ->addIdentifier('id', 'text')
-            ->addIdentifier('title', 'text')
-        ;
-    }
 
     protected function configureFormFields(FormMapper $formMapper)
     {
@@ -55,20 +27,4 @@ class SeoAwareContentAdminController extends Admin{
             ->end()
         ;
     }
-
-    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
-    {
-        $datagridMapper
-            ->add('title', 'doctrine_phpcr_string')
-            ->add('name',  'doctrine_phpcr_nodename')
-        ;
-    }
-
-    public function toString($object)
-    {
-        return $object instanceof StaticContentBase && $object->getTitle()
-            ? $object->getTitle()
-            : $this->trans('link_add', array(), 'SonataAdminBundle')
-            ;
-    }
-} 
+}
