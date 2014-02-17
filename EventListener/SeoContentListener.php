80a4c3fd94ae8330e411b536da437f6af80a4a5d
ElectricMaxxx <maximilian.berghoff@gmx.de>
2014-02-18 00:55:31 +0100

    added the redirect implementation btw and some comments in the Listener

--- EventListener/SeoContentListener.php
+++ EventListener/SeoContentListener.php
@@ -5,8 +5,14 @@ namespace Symfony\Cmf\Bundle\SeoBundle\EventListener;
 use Symfony\Cmf\Bundle\RoutingBundle\Routing\DynamicRouter;
 use Symfony\Cmf\Bundle\SeoBundle\Model\SeoAwareInterface;
 use Symfony\Cmf\Bundle\SeoBundle\Model\SeoPresentationInterface;
+use Symfony\Cmf\Bundle\SeoBundle\Services\TitleParametersLocaleMatcher;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 
+/**
+ * Class SeoContentListener
+ * @package Symfony\Cmf\Bundle\SeoBundle\EventListener
+ */
 class SeoContentListener
 {
     /**
@@ -14,12 +20,17 @@ class SeoContentListener
      */
     private $seoPage;
 
-
+    /**
+     * @param SeoPresentationInterface $seoPage
+     */
     public function __construct(SeoPresentationInterface $seoPage)
     {
         $this->seoPage = $seoPage;
     }
 
+    /**
+     * @param GetResponseEvent $event
+     */
     public function onKernelRequest(GetResponseEvent  $event)
     {
         $contentDocument = $event->getRequest()->attributes->get(DynamicRouter::CONTENT_KEY);
@@ -29,8 +40,7 @@ class SeoContentListener
 
             //have a look if the strategy is redirect and if there is a route to redirect to
             if ($url = $this->seoPage->getRedirect()) {
-                print("should be redirected to $url");
-                exit;
+                $event->setResponse(new RedirectResponse($url));
             }
         }
     }
