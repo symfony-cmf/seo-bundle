2567b630b51711cb0a2b27b602ec8334e0edf259
ElectricMaxxx <maximilian.berghoff@gmx.de>
2014-02-18 21:21:42 +0100

    changed configuration for allowing multilang default title values

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -4,6 +4,7 @@ namespace Symfony\Cmf\Bundle\SeoBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\Config\Definition\Exception\InvalidTypeException;
 
 /**
  * This is the class that validates and merges configuration from your app/config files
@@ -45,6 +46,16 @@ class Configuration implements ConfigurationInterface
                             ->defaultValue('prepend')
                         ->end()
                         ->scalarNode('separator')->defaultValue('')->end()
+                        ->variableNode('default')
+                            ->validate()
+                            ->always(function ($v) {
+                                if (!is_string($v) || !is_array($v)) {
+                                    throw new InvalidTypeException();
+                                }
+                                return $v;
+                            })
+                            ->end()
+                        ->end()
                     ->end()
                 ->end()
                 ->arrayNode('content')
