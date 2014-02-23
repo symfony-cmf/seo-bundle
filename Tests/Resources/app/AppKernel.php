2d2c8c8cda05f350e3736c92619ab5693f2040a2
ElectricMaxxx <maximilian.berghoff@gmx.de>
2014-02-23 09:28:59 +0100

    add routing bundle to test bundles

--- Tests/Resources/app/AppKernel.php
+++ Tests/Resources/app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends TestKernel
         $this->addBundles(array(
             new \Sonata\SeoBundle\SonataSeoBundle(),
             new \Symfony\Cmf\Bundle\SeoBundle\CmfSeoBundle(),
+            new \Symfony\Cmf\Bundle\RoutingBundle\CmfRoutingBundle(),
         ));
     }
 
