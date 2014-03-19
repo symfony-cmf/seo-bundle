6f96882a8b919a3865c722d73dfe9c20a3b55b5c
ElectricMaxxx <maximilian.berghoff@gmx.de>
2014-03-19 12:15:29 +0100

    test for yml + php

---
+++ Tests/Resources/Fixtures/config/multilang_config.php
@@ -0,0 +1,9 @@
+<?php
+$container->loadFromExtension('cmf_seo', array(
+    'title' => array(
+        'default'   => array(
+            'de'    => 'Default Titel',
+            'en'    => 'Default title',
+        ),
+    ),
+));
