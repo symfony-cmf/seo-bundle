6943b6dc7cf9c5a6925800b9965054a257c10d46
ElectricMaxxx <maximilian.berghoff@gmx.de>
2014-03-23 23:34:39 +0100

    complete admin extension stuff

---
+++ Tests/Resources/Admin/SeoAwareContentAdmin.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Symfony\Cmf\Bundle\SeoBundle\Tests\Resources\Admin;
+
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\DoctrinePHPCRAdminBundle\Admin\Admin;
+
+class SeoAwareContentAdmin extends Admin
+{
+
+    protected $baseRouteName = 'seo-content';
+
+    protected $baseRoutePattern = 'seo';
+
+    protected $translationDomain = 'CmfSeoBundle';
+
+    public function getNewInstance()
+    {
+        $new = parent::getNewInstance();
+        if ($this->hasRequest()) {
+            $parentId = $this->getRequest()->query->get('parent');
+            if (null !== $parentId) {
+                $new->setParent($this->getModelManager()->find(null, $parentId));
+            }
+        }
+
+        return $new;
+    }
+
+    public function getExportFormats()
+    {
+        return array();
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('id', 'text')
+            ->addIdentifier('title', 'text')
+        ;
+    }
+
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->with('form.group_general')
+            ->add(
+                'parent',
+                'doctrine_phpcr_odm_tree',
+                array(
+                    'root_node' => $this->getRootPath(),
+                    'choice_list' => array(),
+                    'select_root_node' => true)
+            )
+            ->add('name', 'text')
+            ->add('title', 'text')
+            ->add('body', 'textarea', array('required' => false))
+            ->end()
+        ;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('title', 'doctrine_phpcr_string')
+            ->add('name',  'doctrine_phpcr_nodename')
+        ;
+    }
+}
---
+++ Tests/WebTest/Admin/AdminExtensionTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Symfony\Cmf\Bundle\SeoBundle\Tests\WebTest\Admin;
+
+use Symfony\Cmf\Component\Testing\Functional\BaseTestCase;
+use Symfony\Component\HttpKernel\Client;
+
+/**
+ * This test will cover all behavior with the provides admin extension.
+ *
+ * @author Maximilian Berghoff <Maximilian.Berghoff@gmx.de>
+ */
+class SeoContentAdminExtensionTest extends BaseTestCase
+{
+    /** @var  Client */
+    private $client;
+
+    public function setUp()
+    {
+        $this->db('PHPCR')->loadFixtures(array(
+                'Symfony\Cmf\Bundle\SeoBundle\Tests\Resources\DataFixtures\Phpcr\LoadContentData',
+            ));
+        $this->client = $this->createClient();
+    }
+
+    public function testAdminExtensionExists()
+    {
+        $crawler = $this->client->request('GET', '/admin/seo-content/content-1');
+        $res = $this->client->getResponse();
+
+        $this->assertEquals(200, $res->getStatusCode());
+    }
+}
