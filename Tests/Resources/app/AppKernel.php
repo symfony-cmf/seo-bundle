96eacbb5a41496122ae1ec303281ea4ce3771c8c
ElectricMaxxx <Maximilian.Berghoff@gmx.de>
2014-03-24 14:33:55 +0100

    undo sonata core in test bundle registration

--- Tests/Resources/app/AppKernel.php
+++ Tests/Resources/app/AppKernel.php
@@ -15,7 +15,6 @@ class AppKernel extends TestKernel
 
         $this->addBundles(array(
             new \Sonata\SeoBundle\SonataSeoBundle(),
-            new \Sonata\CoreBundle\SonataCoreBundle(),
             new \Symfony\Cmf\Bundle\SeoBundle\CmfSeoBundle(),
             new \Symfony\Cmf\Bundle\RoutingBundle\CmfRoutingBundle(),
         ));
