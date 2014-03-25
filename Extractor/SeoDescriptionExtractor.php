dc1b922449a661f2ae0b2d2d8879c3f75c96cdec
ElectricMaxxx <maximilian.berghoff@gmx.de>
2014-03-25 22:44:33 +0100

    implement class for "document not found exceptions"

--- Extractor/SeoDescriptionExtractor.php
+++ Extractor/SeoDescriptionExtractor.php
@@ -2,7 +2,7 @@
 
 namespace Symfony\Cmf\Bundle\SeoBundle\Extractor;
 
-use Symfony\Cmf\Bundle\SeoBundle\Exceptions\SeoExtractorStrategyException;
+use Symfony\Cmf\Bundle\SeoBundle\Exceptions\DocumentNotSupport;
 use Symfony\Cmf\Bundle\SeoBundle\Model\SeoAwareInterface;
 use Symfony\Cmf\Bundle\SeoBundle\Model\SeoMetadataInterface;
 
@@ -31,13 +31,9 @@ class SeoDescriptionExtractor implements SeoExtractorInterface
     public function updateMetadata(SeoAwareInterface $document, SeoMetadataInterface $seoMetadata)
     {
         if (!$document instanceof SeoDescriptionInterface) {
-            throw new SeoExtractorStrategyException(
-                sprintf(
-                    'The given document %s is not supported by this strategy. Call supports() method first.',
-                    get_class($document)
-                )
-            );
+            throw new DocumentNotSupport($document);
         }
+
         $seoMetadata->setMetaDescription($document->getSeoDescription());
     }
 }
