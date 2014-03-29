34239a973969c64a750c12335030924e5d6fb9ea
WouterJ <waldio.webdesign@gmail.com>
2014-03-29 19:20:35 +0100

    Added XmlSchema test

---
+++ Tests/Unit/DependencyInjection/UrlSchemaTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Symfony\Cmf\Bundle\SeoBundle\Tests\Unit\DependencyInjection;
+
+use Symfony\Cmf\Component\Testing\Unit\XmlSchemaTestCase;
+
+class XmlSchemaTest extends XmlSchemaTestCase
+{
+    public function testSchema()
+    {
+        $xmlFiles = array_map(function ($file) {
+            return __DIR__.'/../../Resources/Fixtures/config/'.$file;
+        }, array(
+            'config.xml',
+            'config1.xml',
+            'config2.xml',
+        ));
+
+        foreach ($xmlFiles as $xmlFile) {
+            $this->assertSchemaAcceptsXml(array($xmlFile), __DIR__.'/../../../Resources/config/schema/seo-1.0.xsd');
+        }
+    }
+}
+
