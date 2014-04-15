59b88b6a73aebac221b056b3264d03e474aa0d8e
WouterJ <waldio.webdesign@gmail.com>
2014-04-15 16:39:02 +0200

    Fixed cache warming/clearing

--- Cache/CacheInterface.php
+++ Cache/CacheInterface.php
@@ -11,18 +11,13 @@
 
 namespace Symfony\Cmf\Bundle\SeoBundle\Cache;
 
-use Symfony\Component\HttpKernel\CacheWarmer\CacheWarmerInterface;
-
 /**
  * Classes implementing this interface are able to cache extractors for 
  * content objects.
  *
- * Cachers also implement the CacheWarmerInterface, which can be used to 
- * warm up the cache before the application is used.
- *
  * @author Wouter J <wouter@wouterj.nl>
  */
-interface CacheInterface extends CacheWarmerInterface
+interface CacheInterface
 {
     /**
      * Fetches extractors from the cache.
