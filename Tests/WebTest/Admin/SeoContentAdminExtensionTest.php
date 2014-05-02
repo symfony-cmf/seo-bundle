1e1419f4371c075b4f0555d524fcb663bbf3a946
ElectricMaxxx <maximilian.berghoff@gmx.de>
2014-05-02 22:37:00 +0200

    fix test

--- Tests/WebTest/Admin/SeoContentAdminExtensionTest.php
+++ Tests/WebTest/Admin/SeoContentAdminExtensionTest.php
@@ -58,7 +58,7 @@ class SeoContentAdminExtensionTest extends BaseTestCase
 
         $this->assertCount(1, $crawler->filter('html:contains("SEO")'));
         $this->assertCount(1, $crawler->filter('html:contains("Page title")'));
-        $this->assertCount(1, $crawler->filter('html:contains("Original url")'));
+        $this->assertCount(1, $crawler->filter('html:contains("Original URL")'));
         $this->assertCount(1, $crawler->filter('html:contains("description")'));
         $this->assertCount(1, $crawler->filter('html:contains("keywords")'));
     }
