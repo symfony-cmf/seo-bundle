10cfdd5cb6c2bc00b611175366633c8dc27e7572
David Buchmann <david.buchmann@liip.ch>
2014-05-07 20:56:04 +0200

    have admin extension handle the locale

--- Admin/Extension/SeoContentAdminExtension.php
+++ Admin/Extension/SeoContentAdminExtension.php
@@ -12,17 +12,14 @@
 namespace Symfony\Cmf\Bundle\SeoBundle\Admin\Extension;
 
 use Sonata\AdminBundle\Admin\AdminExtension;
+use Sonata\AdminBundle\Admin\AdminInterface;
 use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Cmf\Bundle\CoreBundle\Translatable\TranslatableInterface;
+use Symfony\Cmf\Bundle\SeoBundle\SeoAwareInterface;
 
 /**
  * This AdminExtension will serve the bundle's own form type
  * for configuring seo metadata.
- *
- * To get this admin extension, you need to enable SonataAdminBundle support by
- * setting `cmf_seo.persistence.phpcr.use_sonata_admin` in the configuration to
- * `true` (it defaults to `auto`, which means it'll use it when the
- * SonataAdminBundle is registered). You will need your own Admin class with its
- * mapping and let the content implement the SeoAwareInterface.
  *
  * @author Maximilian Berghoff <maximilian.berghoff@gmx.de>
  */
@@ -35,5 +32,33 @@ class SeoContentAdminExtension extends AdminExtension
                 ->add('seoMetadata', 'seo_metadata', array('label' => false))
             ->end()
         ;
+    }
+
+    public function preUpdate(AdminInterface $admin, $seoAware)
+    {
+        $this->propagateLocale($seoAware);
+    }
+
+    public function prePersist(AdminInterface $admin, $seoAware)
+    {
+        $this->propagateLocale($seoAware);
+    }
+
+    /**
+     * The seo metadata that was edited embedded has the same locale as the
+     * containing document.
+     *
+     * @param SeoAwareInterface $seoAware
+     */
+    private function propagateLocale(SeoAwareInterface $seoAware)
+    {
+        if (!$seoAware instanceof TranslatableInterface) {
+            return;
+        }
+        $seoMetadata = $seoAware->getSeoMetadata();
+        if (!$seoMetadata instanceof TranslatableInterface) {
+            return;
+        }
+        $seoMetadata->setLocale($seoAware->getLocale());
     }
 }
