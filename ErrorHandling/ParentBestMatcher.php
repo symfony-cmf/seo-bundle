e24ef170456a50c1a422e85d4dec1a54a6d9ebfa
ElectricMaxxx <maximilian.berghoff@gmx.de>
2014-08-20 22:10:50 +0200

    fix matcher

--- ErrorHandling/ParentBestMatcher.php
+++ ErrorHandling/ParentBestMatcher.php
@@ -29,16 +29,18 @@ class ParentBestMatcher extends PhpcrBestMatcher
      */
     public function create(Request $request)
     {
-        $routes = new RouteCollection();
+        $routes = array();
         $manager = $this->getManagerForClass('Symfony\Cmf\Bundle\RoutingBundle\Doctrine\Phpcr\Route');
-        $parentPath = PathHelper::getParentPath($this->routeBasePath.'/'.$request->getUri());
+        $parentPath = PathHelper::getParentPath($this->routeBasePath.$request->getPathInfo());
 
-        $parentRoute = $manager->find('Symfony\Cmf\Bundle\RoutingBundle\Doctrine\Phpcr\Route', $parentPath);
+        $parentRoute = $manager->find(null, $parentPath);
         if (!$parentRoute) {
             return $routes;
         }
 
-        $routes->add($parentRoute->getName(), $parentRoute);
+        if ($parentRoute instanceof \Symfony\Component\Routing\Route) {
+            $routes[$parentRoute->getName()] = $parentRoute;
+        }
 
         return $routes;
     }
