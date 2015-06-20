96231fc20eb02e3750f16ebeecf8285063cb4b73
WouterJ <waldio.webdesign@gmail.com>
2015-06-20 17:31:59 +0200

    [2.6] setDefaultOptions -> configureOptions

--- Form/Type/SeoMetadataType.php
+++ Form/Type/SeoMetadataType.php
@@ -13,6 +13,7 @@ namespace Symfony\Cmf\Bundle\SeoBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 /**
@@ -59,6 +60,14 @@ class SeoMetadataType extends AbstractType
      * {@inheritDoc}
      */
     public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $this->configureOptions($resolver);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
             'data_class' => 'Symfony\Cmf\Bundle\SeoBundle\Model\SeoMetadata',
