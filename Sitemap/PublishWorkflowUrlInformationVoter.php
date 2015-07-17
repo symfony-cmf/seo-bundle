b69586184036dc7d405e3a5600bd43b8dced24bf
ElectricMaxxx <maximilian.berghoff@gmx.de>
2015-07-17 08:45:21 +0200

    revert voter changes

--- Sitemap/PublishWorkflowUrlInformationVoter.php
+++ Sitemap/PublishWorkflowUrlInformationVoter.php
@@ -12,8 +12,7 @@
 namespace Symfony\Cmf\Bundle\SeoBundle\Sitemap;
 
 use Symfony\Cmf\Bundle\CoreBundle\PublishWorkflow\PublishWorkflowChecker;
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Core\Authorization\Voter\VoterInterface;
+use Symfony\Cmf\Bundle\SeoBundle\Sitemap\VoterInterface;
 use Symfony\Component\Security\Core\SecurityContextInterface;
 
 /**
@@ -36,38 +35,5 @@ class PublishWorkflowUrlInformationVoter implements VoterInterface
     public function exposeOnSitemap($content, $sitemap = 'default')
     {
         return $this->publishWorkflowChecker->isGranted(PublishWorkflowChecker::VIEW_ATTRIBUTE, $content);
-    }
-
-    /**
-     * {@inheritDocs}
-     */
-    public function supportsAttribute($attribute)
-    {
-        return 'sitemap' === $attribute;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function supportsClass($class)
-    {
-        return true;
-    }
-
-    /**
-     * Returns the vote for the given parameters.
-     *
-     * This method must return one of the following constants:
-     * ACCESS_GRANTED, ACCESS_DENIED, or ACCESS_ABSTAIN.
-     *
-     * @param TokenInterface $token A TokenInterface instance
-     * @param object|null $object The object to secure
-     * @param array $attributes An array of attributes associated with the method being invoked
-     *
-     * @return int either ACCESS_GRANTED, ACCESS_ABSTAIN, or ACCESS_DENIED
-     */
-    public function vote(TokenInterface $token, $object, array $attributes)
-    {
-        // TODO: Implement vote() method.
     }
 }
