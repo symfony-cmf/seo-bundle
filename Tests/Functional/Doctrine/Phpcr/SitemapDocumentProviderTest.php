d38c45badc9a34cd4c27c82fa07749255bf98ddb
ElectricMaxxx <maximilian.berghoff@gmx.de>
2016-04-14 13:43:48 +0200

    add licence header to test file

--- Tests/Functional/Doctrine/Phpcr/SitemapDocumentProviderTest.php
+++ Tests/Functional/Doctrine/Phpcr/SitemapDocumentProviderTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Symfony CMF package.
+ *
+ * (c) 2011-2016 Symfony CMF
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Symfony\Cmf\Bundle\SeoBundle\Tests\Functional\Doctrine\Phpcr;
 
 use Symfony\Cmf\Bundle\SeoBundle\Doctrine\Phpcr\SitemapDocumentProvider;
