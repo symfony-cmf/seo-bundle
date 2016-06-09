343513fe2fbe60018c795cb2933acce666e11fdf
ElectricMaxxx <maximilian.berghoff@gmx.de>
2016-06-09 12:15:50 +0200

    try UTC

--- Tests/Resources/DataFixtures/Phpcr/LoadSitemapData.php
+++ Tests/Resources/DataFixtures/Phpcr/LoadSitemapData.php
@@ -83,7 +83,7 @@ class LoadSitemapData implements FixtureInterface
         $lastModifiedContent = new SitemapAwareWithLastModifiedDateContent();
         $lastModifiedContent
             ->setIsVisibleForSitemap(true)
-            ->setLastModified(new \DateTime('2016-07-06', new \DateTimeZone('Europe/Berlin')))
+            ->setLastModified(new \DateTime('2016-07-06T00:00:00P', new \DateTimeZone('UTC')))
             ->setTitle('Sitemap Aware Content last mod date')
             ->setName('sitemap-aware-last-mod-date')
             ->setParentDocument($contentRoot)
