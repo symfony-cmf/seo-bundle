4127e3b15e4ce89e171df204985b54dcdb9a4c07
Maximilian Berghoff <Maximilian.berghoff@gmx.de>
2017-10-28 15:03:00 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- tests/Unit/DependencyInjection/Compiler/RegisterSuggestionProviderPassTest.php
+++ tests/Unit/DependencyInjection/Compiler/RegisterSuggestionProviderPassTest.php
@@ -43,7 +43,7 @@ class RegisterSuggestionProviderPassTest extends AbstractCompilerPassTestCase
         $this->setDefinition('matcher.with_group', $matcherServiceWithGroup);
 
         $matcherPresentationService = new Definition();
-        $matcherPresentationService->setArguments([1, 1, 2 , 3, []]);
+        $matcherPresentationService->setArguments([1, 1, 2, 3, []]);
         $this->setDefinition('cmf_seo.error.suggestion_provider.controller', $matcherPresentationService);
 
         $this->compile();
@@ -65,7 +65,7 @@ class RegisterSuggestionProviderPassTest extends AbstractCompilerPassTestCase
         $this->setDefinition('matcher.without_group', $matcherServiceWithOutGroup);
 
         $matcherPresentationService = new Definition();
-        $matcherPresentationService->setArguments([1, 1, 2 , 3, []]);
+        $matcherPresentationService->setArguments([1, 1, 2, 3, []]);
         $this->setDefinition('cmf_seo.error.suggestion_provider.controller', $matcherPresentationService);
 
         $this->compile();
